d50ae659aa17f1a4c9ef8bb77fb91f2974e9fbfa
KinjalPatadiya-22 <kinjalpatadiya22@gmail.com>
2022-11-05 21:17:33 +0530

    bug done

--- components/Contactus.tsx
+++ components/Contactus.tsx
@@ -219,7 +219,7 @@ const Contactus = () => {
           />
         </div>
       </div>
-      <a className="absolute bottom-[10px]" href="/#" >
+      <a className="absolute bottom-[30px]" href="/#" >
             <img className="py-6" src="/assets/uparrow.png" alt="arrow" />
           </a>
       </div>
